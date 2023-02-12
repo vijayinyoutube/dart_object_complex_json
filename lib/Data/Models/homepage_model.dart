9b07b23bdcc0be7e55bcfe665db8836918f4c4c7
vijayinyoutube <vijaythevirtue@gmail.com>
2023-02-12 17:19:17 +0530

    refactor

--- lib/Data/Models/homepage_model.dart
+++ lib/Data/Models/homepage_model.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
@@ -6,22 +7,26 @@ class HomePageModel {
   final String name;
   final Map<String, Object> address;
   final Map<String, Object> dob;
+  final List<Map<String, Object>> reviews;
 
   HomePageModel(
     this.name,
     this.address,
     this.dob,
+    this.reviews,
   );
 
   HomePageModel copyWith({
     String? name,
     Map<String, Object>? address,
     Map<String, Object>? dob,
+    List<Map<String, Object>>? reviews,
   }) {
     return HomePageModel(
       name ?? this.name,
       address ?? this.address,
       dob ?? this.dob,
+      reviews ?? this.reviews,
     );
   }
 
@@ -30,14 +35,25 @@ class HomePageModel {
       'name': name,
       'address': address,
       'dob': dob,
+      'reviews': reviews,
     };
   }
 
   factory HomePageModel.fromMap(Map<String, dynamic> map) {
     return HomePageModel(
-        map['name'] as String,
-        Map<String, Object>.from((map['address'] as Map<String, Object>)),
-        Map<String, Object>.from((map['dob'] as Map<String, Object>)));
+      map['name'] as String,
+      Map<String, Object>.from(
+        (map['address'] as Map<String, Object>),
+      ),
+      Map<String, Object>.from(
+        (map['dob'] as Map<String, Object>),
+      ),
+      List<Map<String, Object>>.from(
+        (map['reviews'] as List<dynamic>).map<Map<String, Object>>(
+          (x) => x,
+        ),
+      ),
+    );
   }
 
   String toJson() => json.encode(toMap());
@@ -46,8 +62,9 @@ class HomePageModel {
       HomePageModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() =>
-      'HomePageModel(name: $name, address: $address, dob: $dob)';
+  String toString() {
+    return 'HomePageModel(name: $name, address: $address, dob: $dob, reviews: $reviews)';
+  }
 
   @override
   bool operator ==(covariant HomePageModel other) {
@@ -55,9 +72,12 @@ class HomePageModel {
 
     return other.name == name &&
         mapEquals(other.address, address) &&
-        mapEquals(other.dob, dob);
+        mapEquals(other.dob, dob) &&
+        listEquals(other.reviews, reviews);
   }
 
   @override
-  int get hashCode => name.hashCode ^ address.hashCode ^ dob.hashCode;
+  int get hashCode {
+    return name.hashCode ^ address.hashCode ^ dob.hashCode ^ reviews.hashCode;
+  }
 }
--- lib/Data/Models/json_val.dart
+++ lib/Data/Models/json_val.dart
@@ -5,8 +5,16 @@ var jsonVal = {
     "month": 1,
     "day": 1,
   },
+  "reviews": [
+    {"score": 4.5, "review": "The pizza was amazing!"},
+    {"score": 5.0, "review": "Very friendly staff, excellent service!"}
+  ],
   "address": {
     "city": "City",
+    "reviews": [
+      {"score": 4.5, "review": "The pizza was amazing!"},
+      {"score": 5.0, "review": "Very friendly staff, excellent service!"}
+    ],
     "code": {
       "postcode": 123,
       "zipcode": 111,
